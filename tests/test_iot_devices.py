b4add3efebd5e7e3ea9525a3a801ffc1ee622627
JohnB4000 <jb2069@hw.ac.uk>
2025-01-31 14:01:57 +0000

    Add example test case

--- tests/test_iot_devices.py
+++ tests/test_iot_devices.py
@@ -1,11 +1,46 @@
-import requests
+import csv
+import json
+import requests, pytest
+
+
+@pytest.fixture(autouse=True)
+def config():
+    response = requests.post("http://127.0.0.1:5000/dev/resetdb/").json()
+    assert response == ""
+    yield
 
 
 def test_get_iot_devices():
     response = requests.get("http://127.0.0.1:5000/api/devices/").json()
 
-    # Testing Only
-    response = {"name": "deviceName"}
-    # Testing Only
+    rows = []
+    with open("data/iot_devices.csv", "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows.extend([row for row in reader])
 
-    assert response == {"name": "deviceName"}
+    data = []
+    for row in rows:
+        row["state"] = json.loads(row["state"])
+        for entry in row["state"]:
+            if entry["datatype"] == "integer":
+                entry["value"] = int(entry["value"])
+
+        entry = {
+            "deviceId": int(row["deviceId"]),
+            "name": row["name"],
+            "description": row["description"],
+            "state": row["state"],
+            "status": row["status"],
+            "faultStatus": row["faultStatus"],
+            "pinEnabled": row["pinCode"] != "",
+            "unlocked": bool(row["unlocked"]),
+            "uptimeTimestamp": (
+                row["uptimeTimestamp"] if "uptimeTimestamp" in row else None
+            ),
+            "ipAddress": row["ipAddress"] if "ipAddress" in row else None,
+        }
+        data.append(entry)
+
+    print(data)
+
+    assert response == data
