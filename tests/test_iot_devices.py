95d0f5704fb789834215eeec9093558a863f38d2
JohnB4000 <115543417+JohnB4000@users.noreply.github.com>
2025-02-05 00:18:44 +0000

    Remove redundant parameters

--- tests/test_iot_devices.py
+++ tests/test_iot_devices.py
@@ -1,14 +1,6 @@
 import requests
 
 
-def test_get_iot_devices(
-    iot_devices_data,
-    automations_data,
-    energy_records_data,
-    energy_saving_goals_data,
-    iot_device_usage_data,
-    tags_data,
-    users,
-):
+def test_get_iot_devices(iot_devices_data):
     response = requests.get("http://127.0.0.1:5000/api/devices/").json()
     assert response == iot_devices_data
